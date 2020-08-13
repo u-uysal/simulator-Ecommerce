a7b50ebb41bbc55fca471276ba059506662d0118
ufuk <ufuk.uysl5@gmail.com>
2020-08-13 20:40:21 +0200

    Add pages by using React-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import "./App.css";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Single from "./pages/SingleP";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/products" exact component={Products}></Route>
+        <Route path="/products/:slug" exact component={Single}></Route>
+        <Route component={Error} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
