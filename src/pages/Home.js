11b77934eedcf43ae892b6d9e473f4863d118def
ufuk <ufuk.uysl5@gmail.com>
2020-08-15 12:05:06 +0200

    add sticky sidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -56,154 +56,160 @@ export default function Home() {
     });
   };
   return (
-    <div className="row">
-      {/* sidebar is starting */}
-      <div className="col-sm-3 text-center sidebar">
-        <a
-          className="filters"
-          data-toggle="collapse"
-          href="#collapseOne"
-          aria-expanded="false"
-          aria-controls="collapseOne"
+    <>
+      <div className="row">
+        {/* sidebar is starting */}
+        <div
+          className="col-sm-3 text-center sidebar sticky-top"
+          id="sticky-sidebar"
         >
-          Team <i className="fas fa-arrow-circle-down ml-2"></i>
-        </a>
-        <div className="collapse" id="collapseOne">
-          <div className="card card-body">
-            {" "}
-            {uniqueTeam.map((key, i) => (
-              <RadioTeam
-                key={i}
-                value={key}
-                checked={state.team === key}
-                onChange={handleChange}
-              />
-            ))}
-          </div>
-        </div>
-        <hr></hr>
-        <a
-          className="filters"
-          data-toggle="collapse"
-          href="#collapseTwo"
-          aria-expanded="false"
-          aria-controls="collapseTwo"
-        >
-          Color <i className="fas fa-arrow-circle-down ml-2"></i>
-        </a>
-        <div className="collapse" id="collapseTwo">
-          <div className="card card-body">
-            {uniqueColor.map((key, i) => (
-              <RadioColor
-                key={i}
-                value={key}
-                checked={state.color === key}
-                onChange={handleChange}
-              />
-            ))}
-          </div>
-        </div>
-        <hr></hr>
-        <a
-          className="filters"
-          data-toggle="collapse"
-          href="#collapseThree"
-          aria-expanded="false"
-          aria-controls="collapseThree"
-        >
-          City <i className="fas fa-arrow-circle-down ml-2"></i>
-        </a>
-        <div className="collapse" id="collapseThree">
-          <div className="card card-body">
-            {uniqueCity.map((key, i) => (
-              <RadioCity
-                key={i}
-                value={key}
-                checked={state.city === key}
-                onChange={handleChange}
-              />
-            ))}
-          </div>
-        </div>
-        <hr></hr>
-        <a
-          className="filters"
-          data-toggle="collapse"
-          href="#collapseFour"
-          aria-expanded="false"
-          aria-controls="collapseFour"
-        >
-          Gender <i className="fas fa-arrow-circle-down ml-2"></i>
-        </a>
-        <div className="collapse" id="collapseFour">
-          <div className="card card-body">
-            {" "}
-            {uniqueGender.map((key, i) => (
-              <RadioGender
-                key={i}
-                value={key}
-                checked={state.gender === key}
-                onChange={handleChange}
-              />
-            ))}
-          </div>
-        </div>
-        <hr></hr>
-      </div>
-      {/* sidebar  ended */}
-
-      {/* intro section is starting */}
-
-      <div className="col-sm-9 text-center">
-        <div className="intro">
-          <div className="row">
-            <div className="col-sm-4">
-              <h3 className="font-weight-bold">Laminaat</h3>
-              <p className="text-left">
-                Creëer eenvoudig een natuurlijke sfeer in huis met een laminaat
-                vloer met houtmotief of steenmotief. Je hebt de keuze uit een
-                ruim assortiment. Zo zijn er laminaatvloeren met houtmotief en
-                een voelbare houtstructuur, maar ook dessins met zeer glad
-                leisteen. Deze laminaatvloeren zijn nauwelijks van echt hout of
-                steen te onderscheiden.
-              </p>
-              <h5 className="text-left font-weight-bold">
-                Het gemak van laminaat
-              </h5>
-              <ul className="text-left">
-                <li>
-                  Keuze uit heel veel verschillende houtdessins en steendessins
-                </li>
-                <li>Creëer een natuurlijke sfeer</li>
-                <li>Eenvoudig te plaatsen</li>
-                <li>Gemakkelijk te onderhouden</li>
-              </ul>
-            </div>
-            <div className="col-sm-7 ml-5 main-image"></div>
-            {/* intro section ended */}
-
-            {/* gallery section is starting */}
-            <div className="gallery">
-              {filterArray.map((item, i) => (
-                <div key={i} className="img-part">
-                  <div
-                    style={{ backgroundImage: `url(${img[item.id]})` }}
-                    className="image-display"
-                  ></div>
-                  <div className="image-info">
-                    <p className="text-uppercase">{item.merken}</p>
-                    <span className="text-old">{item.old} &#13217;</span>
-                    <span className="text-new">{item.new} &#13217;</span>
-                    <button type="button" className="btn btn-primary">
-                      Meer informatie
-                    </button>
-                  </div>
-                </div>
+          <a
+            className="filters"
+            data-toggle="collapse"
+            href="#collapseOne"
+            aria-expanded="false"
+            aria-controls="collapseOne"
+          >
+            Team <i className="fas fa-arrow-circle-down ml-2"></i>
+          </a>
+          <div className="collapse" id="collapseOne">
+            <div className="card card-body">
+              {" "}
+              {uniqueTeam.map((key, i) => (
+                <RadioTeam
+                  key={i}
+                  value={key}
+                  checked={state.team === key}
+                  onChange={handleChange}
+                />
               ))}
             </div>
           </div>
+          <hr></hr>
+          <a
+            className="filters"
+            data-toggle="collapse"
+            href="#collapseTwo"
+            aria-expanded="false"
+            aria-controls="collapseTwo"
+          >
+            Color <i className="fas fa-arrow-circle-down ml-2"></i>
+          </a>
+          <div className="collapse" id="collapseTwo">
+            <div className="card card-body">
+              {uniqueColor.map((key, i) => (
+                <RadioColor
+                  key={i}
+                  value={key}
+                  checked={state.color === key}
+                  onChange={handleChange}
+                />
+              ))}
+            </div>
+          </div>
+          <hr></hr>
+          <a
+            className="filters"
+            data-toggle="collapse"
+            href="#collapseThree"
+            aria-expanded="false"
+            aria-controls="collapseThree"
+          >
+            City <i className="fas fa-arrow-circle-down ml-2"></i>
+          </a>
+          <div className="collapse" id="collapseThree">
+            <div className="card card-body">
+              {uniqueCity.map((key, i) => (
+                <RadioCity
+                  key={i}
+                  value={key}
+                  checked={state.city === key}
+                  onChange={handleChange}
+                />
+              ))}
+            </div>
+          </div>
+          <hr></hr>
+          <a
+            className="filters"
+            data-toggle="collapse"
+            href="#collapseFour"
+            aria-expanded="false"
+            aria-controls="collapseFour"
+          >
+            Gender <i className="fas fa-arrow-circle-down ml-2"></i>
+          </a>
+          <div className="collapse" id="collapseFour">
+            <div className="card card-body">
+              {" "}
+              {uniqueGender.map((key, i) => (
+                <RadioGender
+                  key={i}
+                  value={key}
+                  checked={state.gender === key}
+                  onChange={handleChange}
+                />
+              ))}
+            </div>
+          </div>
+          <hr></hr>
+        </div>
+        {/* sidebar  ended */}
+
+        {/* intro section is starting */}
+
+        <div className="col-sm-9 text-center">
+          <div className="intro">
+            <div className="row">
+              <div className="col-sm-4">
+                <h3 className="font-weight-bold">Laminaat</h3>
+                <p className="text-left">
+                  Creëer eenvoudig een natuurlijke sfeer in huis met een
+                  laminaat vloer met houtmotief of steenmotief. Je hebt de keuze
+                  uit een ruim assortiment. Zo zijn er laminaatvloeren met
+                  houtmotief en een voelbare houtstructuur, maar ook dessins met
+                  zeer glad leisteen. Deze laminaatvloeren zijn nauwelijks van
+                  echt hout of steen te onderscheiden.
+                </p>
+                <h5 className="text-left font-weight-bold">
+                  Het gemak van laminaat
+                </h5>
+                <ul className="text-left">
+                  <li>
+                    Keuze uit heel veel verschillende houtdessins en
+                    steendessins
+                  </li>
+                  <li>Creëer een natuurlijke sfeer</li>
+                  <li>Eenvoudig te plaatsen</li>
+                  <li>Gemakkelijk te onderhouden</li>
+                </ul>
+              </div>
+              <div className="col-sm-7 ml-5 main-image"></div>
+              {/* intro section ended */}
+
+              {/* gallery section is starting */}
+              <div className="gallery">
+                {filterArray.map((item, i) => (
+                  <div key={i} className="img-part">
+                    <div
+                      style={{ backgroundImage: `url(${img[item.id]})` }}
+                      className="image-display"
+                    ></div>
+                    <div className="image-info">
+                      <p className="text-uppercase">{item.merken}</p>
+                      <span className="text-old">{item.old} &#13217;</span>
+                      <span className="text-new">{item.new} &#13217;</span>
+                      <button type="button" className="btn btn-primary">
+                        Meer informatie
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
